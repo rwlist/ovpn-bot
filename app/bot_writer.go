267a13cbf272a3ffa8b3944a7046e4f7ce61c41e
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2020-02-09 16:13:21 +0300

    Implement a better flush timeout

--- app/bot_writer.go
+++ app/bot_writer.go
@@ -8,18 +8,27 @@ import (
 
 const flushTimeout = time.Second
 
+type botWriterState uint
+
+const (
+	stateNoop botWriterState = iota
+	stateAwaitingFlush
+)
+
 type botWriter struct {
-	bot *telegram.Bot
+	bot    *telegram.Bot
 	chatID int
 
 	buf []byte
-	m sync.Mutex
+	m   sync.Mutex
+	state botWriterState
 }
 
 func NewBotWriter(bot *telegram.Bot, chatID int) *botWriter {
 	return &botWriter{
 		bot:    bot,
 		chatID: chatID,
+		state:  stateNoop,
 	}
 }
 
@@ -37,19 +46,29 @@ func (w *botWriter) Flush() {
 	w.m.Lock()
 	defer w.m.Unlock()
 
+	w.state = stateNoop
+
 	if len(w.buf) == 0 {
 		return
 	}
 
 	_, _ = w.bot.SendMessage(&telegram.SendMessageRequest{
-		ChatID:                str(w.chatID),
-		Text:                  string(w.buf),
+		ChatID: str(w.chatID),
+		Text:   string(w.buf),
 	})
 
 	w.buf = nil
 }
 
 func (w *botWriter) DeferFlush() {
+	w.m.Lock()
+	defer w.m.Unlock()
+
+	if w.state != stateNoop {
+		return
+	}
+	w.state = stateAwaitingFlush
+
 	time.AfterFunc(flushTimeout, func() {
 		w.Flush()
 	})
